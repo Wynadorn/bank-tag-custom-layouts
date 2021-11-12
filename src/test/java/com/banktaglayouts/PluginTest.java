c0420c9accd87ffeb24b8bfaccd3f288758c3059
Janos <janosdevries@gmail.com>
2021-11-12 14:20:34 +0100

    renamed ExamplePluginTest to PluginText

--- src/test/java/com/banktaglayouts/PluginTest.java
+++ src/test/java/com/banktaglayouts/PluginTest.java
@@ -3,7 +3,7 @@ package com.banktaglayouts;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
